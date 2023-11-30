7ffec42979ad1f09b49beaba9d49659333833abb
Luiz Filipe <luiz.filipe.ctt@gmail.com>
2023-11-30 19:23:50 -0300

    v.1.2

--- data/main.py
+++ data/main.py
@@ -5,14 +5,43 @@ import ttkbootstrap as tb
 
 root = tb.Window(themename='darkly')
 root.title('Salvar Notas')
-root.geometry('600x350')
+root.geometry('500x300')
 root.resizable(False, False)
 root.iconbitmap('./images/SESI.ico')
 
-label_inserir_info = tb.Label(text='Insira seu login e senha:',
+estilo = tb.Style()
+estilo.configure('success.TButton', font=('Bahnschrift', 26))
+
+
+label_login = tb.Label(text='LOGIN:',
                               font=('Bahnschrift light', 26),
-                              bootstyle=LIGHT)
-label_inserir_info.place(x=100,
-                         y=20)
+                              bootstyle=(INFO))
+label_login.place(x=30,
+                y=10)
+
+entry_login = tb.Entry(root,
+                       bootstyle=DARK,
+                       font=('Comic Sans', 20))
+entry_login.place(x=170,
+                  y=10)
+
+label_senha = tb.Label(text='SENHA:',
+                              font=('Bahnschrift light', 26),
+                              bootstyle=(INFO))
+label_senha.place(x=30,
+                y=100)
+
+entry_senha = tb.Entry(root,
+                       bootstyle=DARK,
+                       font=('Comic Sans', 20))
+entry_senha.place(x=170,
+                  y=100)
+
+botao_logar = tb.Button(text='LOGAR',
+                        bootstyle='success',
+                        width=12,
+                        style='success.TButton')
+botao_logar.place(x=130,
+                  y=200)
 
 root.mainloop()
